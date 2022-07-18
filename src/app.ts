1fa52ae4a318461c96afa8a8481d7648df694f49
Jocelyn Jayjack <jocelynjayjack@gmail.com>
2022-07-18 11:51:43 -0500

    Private & Public modifiers

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,28 @@
 class Department {
   name: string;
+  private employees: string[] = []; //private = modifier
   constructor(n: string) {
     this.name = n;
   }
   describe(this: Department) {
     console.log("Department: " + this.name);
   } //Method
+
+  addEmployee(employee: string) {
+    // could add validation
+    this.employees.push(employee);
+  }
+  printEmployeeInformation() {
+    console.log(this.employees.length);
+    console.log(this.employees);
+  }
 }
 const accounting = new Department("Accounting");
+accounting.addEmployee("Max");
+accounting.addEmployee("Amanda");
+// accounting.employees[2] = "Anna"; cannot add on to employees BECAUSE we've set class item as PRIVATE
+
 accounting.describe();
-const accountingCopy = { name: "s", describe: accounting.describe };
-accountingCopy.describe();
+accounting.printEmployeeInformation();
+// const accountingCopy = { name: "s", describe: accounting.describe };
+// accountingCopy.describe();
